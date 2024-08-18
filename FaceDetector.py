ebcd996a6a6a0b91d3c0630975b0b1cc447f5a22
Amayuru1999 <amarasingheau@gmail.com>
2024-08-18 13:23:40 +0530

    feat: Add face detection functionality using OpenCV

--- FaceDetector.py
+++ FaceDetector.py
@@ -3,5 +3,6 @@ import cv2
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 img = cv2.imread('1.jpeg')
+cv2.imshow('Face Detector', img)
 
 print("Code completed 🚀")
