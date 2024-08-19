466a469651e73ca9dace0e6ff3adf0f86e555253
Amayuru1999 <amarasingheau@gmail.com>
2024-08-20 00:11:21 +0530

    Improve face detection functionality by updating bounding box coordinates

--- FaceDetector.py
+++ FaceDetector.py
@@ -1,16 +1,17 @@
 import cv2
+from random import randrange
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('1.jpeg')
+img = cv2.imread('2.jpeg')
 
 grayscaled_img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 face_coordinates= trained_face_data.detectMultiScale(grayscaled_img)
 
 
-(x,y,w,h)=face_coordinates[0]
-cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+for (x,y,w,h) in face_coordinates:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,randrange(255),0),2)
 
 print(face_coordinates)
 
