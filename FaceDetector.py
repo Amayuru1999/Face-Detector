7392059aa7a92a23b1424ee1e1f2ea9da2f0efdd
Amayuru1999 <amarasingheau@gmail.com>
2024-08-19 23:01:45 +0530

    feat: Improve face detection functionality using OpenCV

--- FaceDetector.py
+++ FaceDetector.py
@@ -3,7 +3,11 @@ import cv2
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 img = cv2.imread('1.jpeg')
-cv2.imshow('Face Detector', img)
+
+grayscaled_img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+face_coordinates= trained_face_data.detectMultiScale(grayscaled_img)
+cv2.imshow('Face Detector', grayscaled_img)
 cv2.waitKey()
 
 print("Code completed 🚀")
