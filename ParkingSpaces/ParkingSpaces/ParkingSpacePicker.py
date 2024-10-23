a5333ba84cfe7020432a492caa420dcc7be1e45d
Amayuru1999 <amarasingheau@gmail.com>
2024-10-23 21:55:09 +0530

    chore: Added MouseClick function #2

--- ParkingSpaces/ParkingSpaces/ParkingSpacePicker.py
+++ ParkingSpaces/ParkingSpaces/ParkingSpacePicker.py
@@ -1,17 +1,25 @@
 import cv2
 
 img = cv2.imread('carParkImg.png')
-width,height=107,48
-posList=[]
+width, height = 107, 48
+posList = []
+
 
 def mouseClick(events, x, y, flags, param):
     if events == cv2.EVENT_LBUTTONDOWN:
-        posList.append((x,y))
+        posList.append((x, y))
+
+    if events == cv2.EVENT_RBUTTONDOWN:
+        for i, pos in enumerate(posList):
+            x1, y1 = pos
+            if pos[0] < x < pos[0] + width and pos[1] < y < pos[1] + height:
+                posList.pop(i)
+
 
 while True:
-
+    img = cv2.imread('carParkImg.png')
     for pos in posList:
-        cv2.rectangle(img,pos,(pos[0]+width,pos[1]+height), (255, 0, 255), 2)
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
 
     cv2.imshow("Image", img)
     cv2.setMouseCallback("Image", mouseClick)
