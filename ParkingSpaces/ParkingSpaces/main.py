dd0a438f89ca888fe5a331b1621c0d46eb897b09
Amayuru1999 <amarasingheau@gmail.com>
2024-10-24 20:47:58 +0530

    chore: Added carPark.mp4 #1

--- ParkingSpaces/ParkingSpaces/main.py
+++ ParkingSpaces/ParkingSpaces/main.py
@@ -1,8 +1,11 @@
 import cv2
 
-cap=cv2.VideoCapture('carPark.mp4')
+cap = cv2.VideoCapture('carPark.mp4')
 
 while True:
+
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
     success, img = cap.read()
     cv2.imshow("Video", img)
-    cv2.waitKey(1)
+    cv2.waitKey(10)
