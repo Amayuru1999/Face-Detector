641b53b3df8c686d5090b9b12b1ca876790d2911
Amayuru1999 <amarasingheau@gmail.com>
2024-10-24 20:58:12 +0530

    chore: Added carPark.mp4 #2

--- ParkingSpaces/ParkingSpaces/main.py
+++ ParkingSpaces/ParkingSpaces/main.py
@@ -1,11 +1,25 @@
+import pickle
+
 import cv2
 
+width, height = 107, 48
 cap = cv2.VideoCapture('carPark.mp4')
+
+with open('CarParkPos', 'rb') as f:
+    posList = pickle.load(f)
+
+
+def checkParkingSpace():
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
+
 
 while True:
 
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
     success, img = cap.read()
+    checkParkingSpace()
+
     cv2.imshow("Video", img)
-    cv2.waitKey(1)
+    cv2.waitKey(10)
